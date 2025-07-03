eedee1d2172c819a49a5bc5af0491810be007cf0
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-07-03 15:50:20 +0300

    Add project files.

---
+++ HandmadeITI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HandmadeITI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
