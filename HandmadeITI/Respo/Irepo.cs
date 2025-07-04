96cee24cd83a1afb275d7e3ba3c3d5f30e2d7c16
Ahmed Hammad <ah407397@gmail.com>
2025-07-04 16:21:14 +0300

    Create Irepo and Create ProductRepo

---
+++ HandmadeITI/Respo/Irepo.cs
@@ -0,0 +1,13 @@
+﻿namespace HandmadeITI.Respo
+{
+    public interface Irepo<T> where T : class
+    {
+        Task<T> GetById(int? id);
+        Task<IEnumerable<T>> GetAll();
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(int id);
+        Task SaveChanges();
+    }
+
+}
